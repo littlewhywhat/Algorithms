ca72920b922b8d4f422f57cf06e28592f98667d6
Roman Vayvod <littlewhywhat@gmail.com>
2014-03-24 15:17:25 +0400

    fixed CakeDivider and tests

--- src/com/littlewhywhat/algorithms/cakedivider/CakeDivider.java
+++ src/com/littlewhywhat/algorithms/cakedivider/CakeDivider.java
@@ -1,6 +1,7 @@
 package com.littlewhywhat.algorithms.cakedivider;
 
 import com.littlewhywhat.algorithms.AbstractAlgorithm;
+import com.littlewhywhat.datastructure.divider.SimpleArrayDivider;
 import com.littlewhywhat.datastructure.sieve.SerialArraySieve;
 import com.littlewhywhat.geometry.Figure;
 import com.littlewhywhat.geometry.FigureDosator;
@@ -8,83 +9,13 @@ import com.littlewhywhat.geometry.Geometry;
 import com.littlewhywhat.geometry.Point;
 
 public class CakeDivider extends AbstractAlgorithm<Void, Point[], String[]> {
-	private static final int NUMBER_OF_PARTS = 4;
-	private Point center;
-	private int dividerAngle;
-	private CakeDosator dosator;
-
-	private void setWrongOutput() {
-		setOutput(new String[] { "-1" });
-	}
-
-	private void setRightOutput() {
-		setOutput(new String[] { center.getX() + " " + center.getY(),
-				String.valueOf(dividerAngle) });
-	}
-
-	@Override
-	public void execute() {
-		dosator = new CakeDosator();
-		if (checkPointsCount() && computeIfDividerExists()
-				&& checkCakeSymmetry()) {
-			setRightOutput();
-		} else
-			setWrongOutput();
-	}
-
-	private void computeAngle(Point vertice) {
-		dividerAngle = Geometry.computeAngleByPoints(vertice, center);
-	}
-
-	private boolean computeIfDividerExists() {
-		int count = 0;
-		while (dosator.hasDose()) {
-			dosator.nextDose();
-			
-			if (Geometry.isEqualSided(dosator.getDoseFigure())) {
-				computeCenter(dosator.getDoseFigure());
-				computeAngle(dosator.getDoseFigure().getVertice(0));
-				System.out.println( "square " + count + "\n" +
-						"first " + dosator.getDoseFigure().getPoints()[0] + "\n" + 
-						"second " + dosator.getDoseFigure().getPoints()[1] + "\n" +
-						"third " + dosator.getDoseFigure().getPoints()[2] + "\n" +
-						"fourth " + dosator.getDoseFigure().getPoints()[3] + "\n" +
-						"center " + center
-						);
-				return true;
-			}
-			count++;
+	private class CakeArrayDivider extends SimpleArrayDivider<Point> {
+		public CakeArrayDivider() {
+			super();
+			setArray(getData());
+			setNumberOfParts(NUMBER_OF_PARTS);
 		}
-		return false;
 	}
-
-	private boolean checkCakeSymmetry() {
-		dosator.reset();
-		while (dosator.hasDose()) {
-			dosator.nextDose();
-			if (!Geometry.canBePlacedIntoCircleWithCenter(dosator.getDoseFigure(), center)) {
-				System.out.println(  "can't be placed \n" +
-						"first " + dosator.getDoseFigure().getPoints()[0] + "\n" + 
-						"second " + dosator.getDoseFigure().getPoints()[1] + "\n" +
-						"third " + dosator.getDoseFigure().getPoints()[2] + "\n" +
-						"fourth " + dosator.getDoseFigure().getPoints()[3] + "\n" +
-						"center " + center
-						);
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private void computeCenter(Figure figure) {
-		center = Geometry.computeCircumCenter(figure.getVertice(0),
-				figure.getVertice(1), figure.getVertice(2));
-	}
-
-	private boolean checkPointsCount() {
-		return getData().length % NUMBER_OF_PARTS == 0;
-	}
-
 	private class CakeDosator extends FigureDosator {
 		public CakeDosator() {
 			super();
@@ -92,5 +23,108 @@ public class CakeDivider extends AbstractAlgorithm<Void, Point[], String[]> {
 			setArray(getData());
 			setDoseFigure(NUMBER_OF_PARTS);
 		}
+	}
+	private static final int NUMBER_OF_PARTS = 4;
+	private Point center;
+	private int dividerAngle;
+
+	private CakeDosator dataDosator;
+
+	private CakeArrayDivider dataDivider;
+
+	private boolean checkEachPartPoints() {
+		Point startPoint;
+		Point endPoint;
+		int prevIndex = NUMBER_OF_PARTS - 1;
+		for (int nextIndex = 0; nextIndex < NUMBER_OF_PARTS; nextIndex++) {
+			startPoint = dataDosator.getDoseFigure().getVertice(prevIndex);
+			endPoint = dataDosator.getDoseFigure().getVertice(nextIndex);
+			dataDivider.goToPart(prevIndex);
+			while (dataDivider.partHasItems()) {
+				Point item = dataDivider.getItem();
+				if (!Geometry.checkIfPointIsInAngle(item, center, startPoint,
+						endPoint))
+					return false;
+			}
+			prevIndex = nextIndex;
+		}
+		return true;
+	}
+
+	private boolean checkEachPartSquares() {
+		double firstPartSquare = Geometry.computeSquare(getPartPoints(0));
+		double otherPartSquare;
+		for (int part = 1; part < NUMBER_OF_PARTS; part++) {
+			Point[] partPoints = getPartPoints(part);
+			otherPartSquare = Geometry.computeSquare(partPoints);
+			if (otherPartSquare != firstPartSquare)
+				return false;
+		}
+		return true;
+	}
+
+	private boolean checkPointsCount() {
+		return getData().length % NUMBER_OF_PARTS == 0;
+	}
+
+	private void computeAngle(Point vertice) {
+		dividerAngle = Geometry.computeAngleByPoints(vertice, center);
+	}
+
+	private void computeCenter(Figure figure) {
+		center = Geometry.computeCircumCenter(figure.getVertice(0),
+				figure.getVertice(1), figure.getVertice(2));
+	}
+
+	private boolean computeIfDividerExists() {
+		int count = -1;
+		while (dataDosator.hasDose()) {
+			count++;
+			dataDosator.nextDose();
+			Figure dose = dataDosator.getDoseFigure();
+			if (!Geometry.isEqualSided(dose))
+				continue;
+			computeCenter(dose);
+			computeAngle(dose.getVertice(0));
+			dataDivider.setStartIndex(count);
+			if (!checkEachPartPoints())
+				continue;
+			if (!checkEachPartSquares())
+				continue;
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void execute() {
+		dataDosator = new CakeDosator();
+		dataDivider = new CakeArrayDivider();
+		if (checkPointsCount() && computeIfDividerExists()) {
+			setRightOutput();
+		} else
+			setWrongOutput();
+	}
+
+	private Point[] getPartPoints(int part) {
+		int count = 0;
+		Point[] partPoints = new Point[getData().length / NUMBER_OF_PARTS + 2];
+		dataDivider.goToPart(part);
+		while (dataDivider.partHasItems()) {
+			partPoints[count] = dataDivider.getItem();
+			count++;
+		}
+		partPoints[partPoints.length - 2] = dataDivider.getItem();
+		partPoints[partPoints.length - 1] = center;
+		return partPoints;
+	}
+
+	private void setRightOutput() {
+		setOutput(new String[] { center.getX() + " " + center.getY(),
+				String.valueOf(dividerAngle) });
+	}
+
+	private void setWrongOutput() {
+		setOutput(new String[] { "-1" });
 	}
 }
