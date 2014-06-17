18179fdc22cf886937850c2fb173736c2b32d85c
Roman Vayvod <littlewhywhat@gmail.com>
2014-06-17 14:19:05 +0400

    refactored DijkstraGraphFiller

--- src/com/littlewhywhat/algorithms/graphs/dijsktra/DijkstraGraphFiller.java
+++ src/com/littlewhywhat/algorithms/graphs/dijsktra/DijkstraGraphFiller.java
@@ -11,25 +11,55 @@ import com.littlewhywhat.algorithms.graphs.io.GraphFiller;
 
 public class DijkstraGraphFiller implements GraphFiller {
 
-	@Override
-	public void fill(Graph graph, Scanner scanner) {
-		Scanner line;
-		while (scanner.hasNextLine()) {
-			line = new Scanner(scanner.nextLine());
-			int verticeIndex = line.nextInt() - 1;
-			while (line.hasNext()) {
-				String lineConnectionWithWeight = line.next();
-				String[] connectionWithWeight = StringUtils.split(
-						lineConnectionWithWeight, ',');
-				int connectionIndex = Integer.valueOf(connectionWithWeight[0]) - 1;
-				int weight = Integer.valueOf(connectionWithWeight[1]);
-				graph.connect(verticeIndex, connectionIndex);
-				DijkstraVertice vertice = ((DijkstraVertice) graph.getVertice(verticeIndex));
-				Connection connection = vertice.getConnectionByIndex(connectionIndex);
-				connection.setWeight(weight);
-			}
-		}
+	private class ConnectionInfo {
+		private int connectionIndex;
+		private int weight;
 
+		private ConnectionInfo(int connectionIndex, int weight) {
+			this.connectionIndex = connectionIndex;
+			this.weight = weight;
+		}
 	}
 
+	private static final int CONNECTION_INFO_INDEX = 0;
+
+	private static final int WEIGHT_INFO_INDEX = 1;
+
+	private int convertVerticeIndex(int realIndex) {
+		return realIndex - 1;
+	}
+
+	@Override
+	public void fill(Graph graph, Scanner scanner) {
+		while (scanner.hasNextLine())
+			try (Scanner line = new Scanner(scanner.nextLine())) {
+				processLine(graph, line);
+			}
+	}
+
+	private Connection getConnectionFromGraph(Graph graph, int verticeIndex,
+			int connectionIndex) {
+		DijkstraVertice vertice = ((DijkstraVertice) graph
+				.getVertice(verticeIndex));
+		return vertice.getConnectionByIndex(connectionIndex);
+	}
+
+	private ConnectionInfo getConnectionInfoFromToken(String token) {
+		String[] info = StringUtils.split(token, ',');
+		return new ConnectionInfo(
+				convertVerticeIndex(Integer
+						.valueOf(info[CONNECTION_INFO_INDEX])),
+				Integer.valueOf(info[WEIGHT_INFO_INDEX]));
+	}
+
+	private void processLine(Graph graph, Scanner line) {
+		final int verticeIndex = convertVerticeIndex(line.nextInt());
+		while (line.hasNext()) {
+			ConnectionInfo info = getConnectionInfoFromToken(line.next());
+			graph.connect(verticeIndex, info.connectionIndex);
+			Connection connection = getConnectionFromGraph(graph, verticeIndex,
+					info.connectionIndex);
+			connection.setWeight(info.weight);
+		}
+	}
 }
