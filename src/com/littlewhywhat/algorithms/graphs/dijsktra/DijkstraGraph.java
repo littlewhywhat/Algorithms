f4f85f1f7820836d0393b8b76e496464c1ce2bb3
Roman Vayvod <littlewhywhat@gmail.com>
2014-06-17 15:10:45 +0400

    made class public

--- src/com/littlewhywhat/algorithms/graphs/dijsktra/DijkstraGraph.java
+++ src/com/littlewhywhat/algorithms/graphs/dijsktra/DijkstraGraph.java
@@ -3,7 +3,7 @@ package com.littlewhywhat.algorithms.graphs.dijsktra;
 import com.littlewhywhat.algorithms.graphs.Vertice;
 import com.littlewhywhat.algorithms.graphs.mincut.contraction.ContractionGraph;
 
-class DijkstraGraph extends ContractionGraph {
+public class DijkstraGraph extends ContractionGraph {
 	class DijkstraVertice extends ContractionVertice {
 
 		protected int intValue;
