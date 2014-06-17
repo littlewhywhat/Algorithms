e41391a030e80783d0f117db0163d660b63aaa9b
Roman Vayvod <littlewhywhat@gmail.com>
2014-06-17 18:59:00 +0400

    SimpleConnectionsList : made to implements ConnectionsList

--- src/com/littlewhywhat/algorithms/graphs/SimpleConnectionsList.java
+++ src/com/littlewhywhat/algorithms/graphs/SimpleConnectionsList.java
@@ -1,80 +1,30 @@
 package com.littlewhywhat.algorithms.graphs;
 
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
-public class SimpleConnectionsList implements Iterable<Vertice>, Collection<Vertice> {
+class SimpleConnectionsList implements ConnectionsList {
 
 	private final List<Vertice> list;
 	
-	public SimpleConnectionsList(List<Vertice> list) {
+	SimpleConnectionsList(List<Vertice> list) {
 		this.list = list;
-	}
-	
-	@Override
-	public boolean add(Vertice vertice) {
-		return list.add(vertice);
-	}
-
-	@Override
-	public boolean addAll(Collection<? extends Vertice> arg0) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void clear() {
-		this.list.clear();
-	}
-
-	@Override
-	public boolean contains(Object arg0) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean containsAll(Collection<?> arg0) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean isEmpty() {
-		return this.list.isEmpty();
-	}
-
-	@Override
-	public boolean remove(Object arg0) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean removeAll(Collection<?> arg0) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean retainAll(Collection<?> arg0) {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public int size() {
-		return this.list.size();
-	}
-
-	@Override
-	public Object[] toArray() {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public <T> T[] toArray(T[] arg0) {
-		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Iterator<Vertice> iterator() {
-		return this.list.iterator();
+		return list.iterator();
 	}
+
+	@Override
+	public Vertice get(int index) {
+		return list.get(index);
+	}
+
+	@Override
+	public int size() {
+		return list.size();
+	}
+	
 
 }
