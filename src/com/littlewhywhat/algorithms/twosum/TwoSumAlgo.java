e860ba05ff61f6bb26494a8c6318ec5589d57247
Roman Vayvod <littlewhywhat@gmail.com>
2014-06-19 15:51:22 +0400

    TwoSumAlgo: added

---
+++ src/com/littlewhywhat/algorithms/twosum/TwoSumAlgo.java
@@ -0,0 +1,14 @@
+package com.littlewhywhat.algorithms.twosum;
+
+import com.littlewhywhat.algorithms.AbstractAlgorithm;
+
+public class TwoSumAlgo extends
+		AbstractAlgorithm<TwoSumConfig, int[], Integer> {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
