b9629b656a7fd8dd1577ad7ed79f1c150b023395
Roman Vayvod <littlewhywhat@gmail.com>
2014-06-19 15:52:00 +0400

    TestTwoSum: added

---
+++ src/com/littlewhywhat/algorithms/twosum/test/TestTwoSum.java
@@ -0,0 +1,19 @@
+package com.littlewhywhat.algorithms.twosum.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestTwoSum {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
