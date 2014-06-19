25fbb6340bacf9aaf4022afa9d11f6cffad25885
Roman Vayvod <littlewhywhat@gmail.com>
2014-06-19 17:21:13 +0400

    TwoSumAlgo: fixed issues in computing

--- src/com/littlewhywhat/algorithms/twosum/TwoSumAlgo.java
+++ src/com/littlewhywhat/algorithms/twosum/TwoSumAlgo.java
@@ -4,11 +4,10 @@ import java.util.HashSet;
 
 import com.littlewhywhat.algorithms.AbstractAlgorithm;
 
-public class TwoSumAlgo extends
-		AbstractAlgorithm<TwoSumConfig, int[], Integer> {
+public class TwoSumAlgo extends AbstractAlgorithm<TwoSumConfig, int[], Integer> {
 
 	private final HashSet<Integer> hashSet = new HashSet<Integer>();
-	
+
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
@@ -17,12 +16,14 @@ public class TwoSumAlgo extends
 		final int upperBound = getConfig().getUpperBound();
 		final int lowerBound = getConfig().getLowerBound();
 		for (int integer : getData()) {
-			int result;
-			hashSet.remove(integer);
-			for (int inBorders = lowerBound; inBorders <= upperBound; inBorders++) {
-				result = lowerBound - integer;
-				if (hashSet.contains(result) && !hashSet.contains(integer))
-					count++;
+			if (hashSet.contains(integer)) {
+				hashSet.remove(integer);
+				int result;
+				for (int inBorders = lowerBound; inBorders <= upperBound; inBorders++) {
+					result = inBorders - integer;
+					if (hashSet.contains(result) && !hashSet.contains(integer))
+						count++;
+				}
 			}
 		}
 		setOutput(count);
