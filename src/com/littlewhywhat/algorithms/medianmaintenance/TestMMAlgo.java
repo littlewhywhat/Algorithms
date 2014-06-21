1f4ecacf6959b7ee33a6560c220dd0781085cdd9
Roman Vayvod <littlewhywhat@gmail.com>
2014-06-21 15:03:02 +0400

    TestMMAlgo : implemented

--- src/com/littlewhywhat/algorithms/medianmaintenance/TestMMAlgo.java
+++ src/com/littlewhywhat/algorithms/medianmaintenance/TestMMAlgo.java
@@ -1,11 +1,19 @@
 package com.littlewhywhat.algorithms.medianmaintenance;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.littlewhywhat.algorithms.Algorithm;
+import com.littlewhywhat.algorithms.io.txt.FileInputReader;
+import com.littlewhywhat.algorithms.sort.SortingReader;
+
 public class TestMMAlgo {
+
+	private final String FOLDER = "src/com/littlewhywhat/algorithms/medianmaintenance/test/input/";
+	private final String INPUT_BIG = FOLDER + "Median.txt";
+	private final FileInputReader<Void, int[]> reader = new SortingReader();
+	private final Algorithm<Void, int[], Integer> algo = new MMAlgo();
 
 	@Before
 	public void setUp() throws Exception {
@@ -13,7 +21,11 @@ public class TestMMAlgo {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		reader.setInputFilePath(INPUT_BIG);
+		reader.read();
+		algo.setData(reader.getData());
+		algo.execute();
+		Assert.assertEquals(null, algo.getOutput());
 	}
 
 }
