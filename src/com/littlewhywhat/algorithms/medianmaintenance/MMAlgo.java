15aa7480442748c9edb8bb22b28e78f4c9b2eecd
Roman Vayvod <littlewhywhat@gmail.com>
2014-06-21 14:54:10 +0400

    MMAlgo : added

---
+++ src/com/littlewhywhat/algorithms/medianmaintenance/MMAlgo.java
@@ -0,0 +1,13 @@
+package com.littlewhywhat.algorithms.medianmaintenance;
+
+import com.littlewhywhat.algorithms.AbstractAlgorithm;
+
+public class MMAlgo extends AbstractAlgorithm<Void, int[], Integer> {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
