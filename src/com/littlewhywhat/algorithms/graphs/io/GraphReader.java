d4bb2fcf5500ab43d4b6ef16b4ce544bcfa34a03
Roman Vayvod <littlewhywhat@gmail.com>
2014-06-24 19:22:48 +0400

    GraphReader: made abstract, made to extend TextFileInputReader, removed fields, added protected abstract fillGraph method

--- src/com/littlewhywhat/algorithms/graphs/io/GraphReader.java
+++ src/com/littlewhywhat/algorithms/graphs/io/GraphReader.java
@@ -1,41 +1,22 @@
 package com.littlewhywhat.algorithms.graphs.io;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import com.littlewhywhat.algorithms.graphs.Graph;
-import com.littlewhywhat.algorithms.io.txt.AbstractFileInputReader;
+import com.littlewhywhat.algorithms.io.txt.TextFileInputReader;
 
-public class GraphReader extends AbstractFileInputReader<Void, Graph> {
-
-	private SizeCounter sizeCounter;
-	private GraphFiller filler;
+public abstract class GraphReader extends TextFileInputReader<Void, Graph> {
 
 	public void setGraph(Graph graph) {
 		this.setData(graph);
 	}
 
-	public void setSizeCounter(SizeCounter sizeCounter) {
-		this.sizeCounter = sizeCounter;
-	}
-
-	public void setGraphFiller(GraphFiller filler) {
-		this.filler = filler;
-	}
-
 	@Override
-	protected void readFile(File file) {
-		try (Scanner countScanner = new Scanner(file);
-				Scanner fillerScanner = new Scanner(file)) {
-			sizeCounter.count(countScanner);
-			getData().setSize(sizeCounter.getSize());
-			filler.fill(getData(), fillerScanner);
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found: " + this.getInputFilePath());
-		} catch (InputMismatchException e) {
-			System.out.println("Unable to read file: " + this.getInputFilePath());
-		}
+	protected void extractInputData(Scanner scanner)
+			throws InputMismatchException {
+		fillGraph(getData(), scanner);
 	}
+
+	protected abstract void fillGraph(Graph data, Scanner scanner);
 }
