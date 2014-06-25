f2807f217bc7ba08857377bdfc1c07ec906122b9
Roman Vayvod <littlewhywhat@gmail.com>
2014-06-25 16:05:37 +0400

    RandomContraction: added vertices for proper randomization

--- src/com/littlewhywhat/algorithms/graphs/mincut/contraction/RandomContraction.java
+++ src/com/littlewhywhat/algorithms/graphs/mincut/contraction/RandomContraction.java
@@ -1,8 +1,10 @@
 package com.littlewhywhat.algorithms.graphs.mincut.contraction;
 
+import java.util.LinkedList;
 import java.util.Random;
 
 import com.littlewhywhat.algorithms.AbstractAlgorithm;
+import com.littlewhywhat.algorithms.graphs.Graph;
 import com.littlewhywhat.algorithms.graphs.Vertice;
 import com.littlewhywhat.algorithms.graphs.mincut.contraction.ContractionGraph.ContractionVertice;
 
@@ -14,23 +16,32 @@ public class RandomContraction extends
 	@Override
 	public void execute() {
 		ContractionGraph graph = getData();
+		final LinkedList<Vertice> vertices = getVertices(graph);
 		int end = graph.size() - 2;
 		for (int i = 0; i < end; i++) {
-			ContractionVertice randomStart = getRandomVertice();
-			ContractionVertice randomEnd = getRandomConnection(randomStart);
+			ContractionVertice randomStart = getRandomVertice(vertices);
+			ContractionVertice randomEnd = getRandomConnection(graph, randomStart);
 			graph.merge(randomStart, randomEnd);
 		}
-		setOutput(graph.getVertice(0).getConnections().size());
+		setOutput(graph.getConnections(vertices.peek()).size());
 	}
 
-	private ContractionVertice getRandomConnection(Vertice vertice) {
-		return (ContractionVertice) vertice.getConnections().get(random
-				.nextInt(vertice.getConnections().size()));
+	private ContractionVertice getRandomVertice(LinkedList<Vertice> vertices) {
+		ContractionVertice vertice = (ContractionVertice) vertices.get(random.nextInt(vertices.size()));
+		vertices.remove(vertice);
+		return vertice;
 	}
 
-	private ContractionVertice getRandomVertice() {
-		return (ContractionVertice) getData().getVertice(
-				random.nextInt(getData().size()));
+	private LinkedList<Vertice> getVertices(ContractionGraph graph) {
+		final LinkedList<Vertice> list = new LinkedList<Vertice>();
+		for (Vertice vertice: graph)
+			list.add(vertice);
+		return list;
+	}
+
+	private ContractionVertice getRandomConnection(Graph graph, Vertice vertice) {
+		return (ContractionVertice) ((ContractionGraph) graph).getConnections(vertice).get(random
+				.nextInt(((ContractionGraph) graph).getConnections(vertice).size()));
 	}
 
 }
