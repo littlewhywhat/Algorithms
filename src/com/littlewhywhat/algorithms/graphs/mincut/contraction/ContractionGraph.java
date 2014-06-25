3e724b8a80921f7110ea2c265475609f68c5fe08
Roman Vayvod <littlewhywhat@gmail.com>
2014-06-25 14:12:43 +0400

    ContractionGraph: made to extend SimpleGraph, changed a lot

--- src/com/littlewhywhat/algorithms/graphs/mincut/contraction/ContractionGraph.java
+++ src/com/littlewhywhat/algorithms/graphs/mincut/contraction/ContractionGraph.java
@@ -1,86 +1,50 @@
 package com.littlewhywhat.algorithms.graphs.mincut.contraction;
 
-import java.util.List;
+import java.util.Iterator;
+import java.util.LinkedList;
 
-import com.littlewhywhat.algorithms.graphs.ConnectionsList;
-import com.littlewhywhat.algorithms.graphs.LinkedGraph;
-import com.littlewhywhat.algorithms.graphs.SimpleConnectionsList;
+import com.littlewhywhat.algorithms.graphs.SimpleGraph;
 import com.littlewhywhat.algorithms.graphs.Vertice;
 
-public class ContractionGraph extends LinkedGraph {
+public class ContractionGraph extends SimpleGraph {
 
 	public class ContractionVertice extends SimpleVertice {
 
+		private LinkedList<ContractionVertice> feedback = new LinkedList<ContractionVertice>();
+		
 		public ContractionVertice(int index) {
 			super(index);
 		}
 
-		private ContractionVertice mergedTo;
-
-		private ContractionVertice leader() {
-			ContractionVertice leader = this;
-			while (leader.mergedTo != null)
-				leader = leader.mergedTo;
-			return leader;
+		private void changeConnection(ContractionVertice old,
+				ContractionVertice fresh) {
+			while(feedback.remove(old))
+				feedback.add(fresh);
+			while(getLinkedConnections(this).remove(old))
+				getLinkedConnections(this).add(fresh);
 		}
 
-		@Override
-		public String toString() {
-			return "[" + super.toString() + ">" + leader().getIndex() + "]";
+		private void removeSelfLoops() {
+			Iterator<Vertice> iterator = getLinkedConnections(this).listIterator();
+			while (iterator.hasNext())
+				if (iterator.next().equals(this))
+					iterator.remove();
+			Iterator<ContractionVertice> fbIterator = feedback.listIterator();
+			while (fbIterator.hasNext())
+				if (fbIterator.next().equals(this))
+					fbIterator.remove();
 		}
 
-		private void addConnectionsOf(ContractionVertice one) {
-			for (Vertice vertice : one.getConnections())
-				if (!((ContractionVertice) vertice).leaderEquals(this))
-					ContractionGraph.this.getConnections(this).addLast(vertice);
-		}
-
-		protected boolean leaderEquals(ContractionVertice contractionVertice) {
-			return ((ContractionVertice) this).leader().equals(contractionVertice);
-		}
-
-		protected void setMergedTo(ContractionVertice two) {
-			this.mergedTo = two;
-		};
-
-	}
-
-	class ContractionConnections extends SimpleConnectionsList {
-
-		ContractionConnections(List<Vertice> list) {
-			super(list);
-		}
-
-		@Override
-		public Vertice get(int index) {
-			return ((ContractionVertice)super.get(index)).leader();
-		}
-		
-	}
-	
-	public ContractionGraph() {
-		super();
-	}
-
-	public ContractionGraph(int size) {
-		super(size);
 	}
 
 	public void merge(ContractionVertice one, ContractionVertice two) {
-		one.setMergedTo(two);
-		clearConnections(two);
-		two.addConnectionsOf(one);
-		this.getConnections(one).clear();
-	}
-
-	private void clearConnections(ContractionVertice vertice) {
-		int size = vertice.getConnections().size();
-		for (int i = 0; i < size; i++) {
-			ContractionVertice connection = (ContractionVertice) this
-					.getConnections(vertice).pollFirst();
-			if (!connection.leaderEquals(vertice))
-				this.getConnections(vertice).addLast(connection);
-		}
+		for (ContractionVertice vertice: one.feedback)
+			vertice.changeConnection(one, two);
+		getLinkedConnections(two).addAll(getLinkedConnections(one));
+		two.feedback.addAll(one.feedback);
+		two.removeSelfLoops();
+		getVertices().remove(one.getIndex());
+		getLinkedConnections(one).clear();
 	}
 
 	@Override
@@ -89,16 +53,13 @@ public class ContractionGraph extends LinkedGraph {
 	}
 
 	@Override
-	public Vertice getVertice(int index) {
-		return ((ContractionVertice) super.getVertice(index)).leader();
+	public void connect(int one, int two) {
+		super.connect(one, two);
+		ContractionVertice verticeOne = (ContractionVertice) get(one);
+		ContractionVertice verticeTwo = (ContractionVertice) get(two);
+		verticeTwo.feedback.add(verticeOne);
 	}
 
-	protected Vertice getVerticeSimple(int index) {
-		return super.getVertice(index);
-	}
 	
-	@Override
-	protected ConnectionsList getNewConnectionsList(List<Vertice> list) {
-		return new ContractionConnections(list);
-	}
+	
 }
