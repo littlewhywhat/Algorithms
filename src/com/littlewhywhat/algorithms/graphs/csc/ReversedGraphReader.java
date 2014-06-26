86478a177541ac46e8a8759809a73e91eb86aa16
Roman Vayvod <littlewhywhat@gmail.com>
2014-06-26 23:12:44 +0400

    ReversedGraphReader: formated

--- src/com/littlewhywhat/algorithms/graphs/csc/ReversedGraphReader.java
+++ src/com/littlewhywhat/algorithms/graphs/csc/ReversedGraphReader.java
@@ -9,7 +9,6 @@ public class ReversedGraphReader extends GraphReader {
 
 	@Override
 	protected void fillGraph(Graph graph, Scanner scanner) {
-		// TODO Auto-generated method stub
 		while (scanner.hasNext()) {
 			int two = scanner.nextInt() - 1;
 			int one = scanner.nextInt() - 1;
