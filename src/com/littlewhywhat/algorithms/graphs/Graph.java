3b753ffb4e715d924ff1f113ba029b4200ab309e
Roman Vayvod <littlewhywhat@gmail.com>
2014-07-04 20:41:40 +0400

    Graph: made T to extend Id

--- src/com/littlewhywhat/algorithms/graphs/Graph.java
+++ src/com/littlewhywhat/algorithms/graphs/Graph.java
@@ -3,7 +3,7 @@ package com.littlewhywhat.algorithms.graphs;
 import java.util.Iterator;
 import java.util.List;
 
-public interface Graph<T> extends List<T> {
+public interface Graph<T extends Id> extends List<T> {
 	void connect(int one, int two);
 	Iterator<T> getConnections(T item);
 }
