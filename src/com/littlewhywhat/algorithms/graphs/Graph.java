adaf95656f9558a9b87a97349cbb64ed85266b07
Roman Vayvod <littlewhywhat@gmail.com>
2014-07-04 21:07:21 +0400

    Graph: changed Iterator to ListIterator

--- src/com/littlewhywhat/algorithms/graphs/Graph.java
+++ src/com/littlewhywhat/algorithms/graphs/Graph.java
@@ -1,10 +1,10 @@
 package com.littlewhywhat.algorithms.graphs;
 
 import java.util.Collection;
-import java.util.Iterator;
+import java.util.ListIterator;
 
 public interface Graph<T extends Id> extends Collection<T> {
 	void connect(int one, int two);
-	Iterator<T> getConnections(T item);
+	ListIterator<T> getConnections(T item);
 	T get(int id);
 }
