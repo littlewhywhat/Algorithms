ca440c3151d11452ee8ee9da266d7ad62251b0ff
Roman Vayvod <littlewhywhat@gmail.com>
2014-07-04 23:55:41 +0400

    TestSimpleDirectedGraph: implemented tests for all methods of Graph, except getConnections, connect, addAll, iterator, reverse

--- src/com/littlewhywhat/algorithms/graphs/test/TestSimpleDirectedGraph.java
+++ src/com/littlewhywhat/algorithms/graphs/test/TestSimpleDirectedGraph.java
@@ -1,5 +1,8 @@
 package com.littlewhywhat.algorithms.graphs.test;
 
+import java.util.Random;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,14 +12,14 @@ import com.littlewhywhat.algorithms.graphs.SimpleDirectedGraph;
 
 public class TestSimpleDirectedGraph {
 
-	private class Item implements Id{
+	private class Item implements Id {
 
 		private int id;
-		
+
 		private Item(int id) {
 			this.id = id;
 		}
-		
+
 		@Override
 		public int getId() {
 			return id;
@@ -26,21 +29,146 @@ public class TestSimpleDirectedGraph {
 		public String toString() {
 			return String.valueOf(id);
 		}
-		
-		
+
+		@Override
+		public int hashCode() {
+			return id;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (!(obj instanceof Item)) {
+				return false;
+			}
+			Item other = (Item) obj;
+			if (id != other.id) {
+				return false;
+			}
+			return true;
+		}
+
+
 	}
-	
-	private final Graph<Item> graph = new SimpleDirectedGraph<Item>();
-	
+
+	private Graph<Item> graph;
+	private final Item[] items = new Item[] { new Item(0), new Item(1),
+			new Item(2), new Item(3) };
+	private Random random = new Random();
+
 	@Before
 	public void setUp() throws Exception {
-		graph.add(new Item(1));
-		
+		graph = new SimpleDirectedGraph<Item>();
 	}
 
 	@Test
-	public void test() {
-		graph.add(new Item(1));
+	public void testReverse() {
+		Assert.fail("Not implemented");
 	}
 
+	@Test
+	public void testSize() {
+		int count = 0;
+		Assert.assertEquals(count, graph.size());
+		for (Item item : items) {
+			graph.add(item);
+			Assert.assertEquals(++count, graph.size());
+		}
+		for (Item item : items) {
+			graph.remove(item.getId());
+			Assert.assertEquals(--count, graph.size());
+		}
+	}
+
+	@Test
+	public void testRemove() {
+		addAllItems();
+		final int index = 0;
+		final int id = items[index].getId();
+		graph.remove(id);
+		Assert.assertEquals(null, graph.get(id));
+		Assert.assertEquals(items.length - 1, graph.size());
+	}
+
+	private void addAllItems() {
+		for (Item item : items) {
+			graph.add(item);
+		}
+	}
+
+	@Test
+	public void testIterator() {
+		Assert.fail("Not implemented");
+	}
+
+	@Test
+	public void testIsEmpty() {
+		final Item item = items[0];
+		Assert.assertEquals(true, graph.isEmpty());
+		graph.add(item);
+		Assert.assertEquals(false, graph.isEmpty());
+		graph.remove(item.getId());
+		Assert.assertEquals(true, graph.isEmpty());
+	}
+
+	@Test
+	public void testContains() {
+		final Item item = getRandomItem();
+		addAllItems();
+		Assert.assertEquals(true, graph.contains(item.getId()));
+		graph.remove(item.getId());
+		Assert.assertEquals(false, graph.contains(item.getId()));
+	}
+
+	@Test
+	public void testClear() {
+		Assert.assertEquals(true, graph.isEmpty());
+		addAllItems();
+		Assert.assertEquals(false, graph.isEmpty());
+		graph.clear();
+		Assert.assertEquals(true, graph.isEmpty());
+	}
+
+	@Test
+	public void testAddAll() {
+		Assert.fail("Not implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		for (Item item : items)
+			Assert.assertEquals(true, graph.add(item));
+		Assert.assertEquals(items.length, graph.size());
+		for (Item item : items)
+			Assert.assertEquals(false, graph.add(item));
+		Assert.assertEquals(items.length, graph.size());
+		for (Item item : items)
+			Assert.assertEquals(true, graph.contains(item.getId()));
+	}
+
+	private Item getRandomItem() {
+		return items[random.nextInt(items.length)];
+	}
+
+	@Test
+	public void testGet() {
+		addAllItems();
+		for (Item item : items)
+			Assert.assertEquals(item, graph.get(item.getId()));
+	}
+
+	@Test
+	public void testGetConnections() {
+		Assert.fail("Not implemented");
+	}
+
+	@Test
+	public void testConnect() {
+		Assert.fail("Not implemented");
+	}
 }
