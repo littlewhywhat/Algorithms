5e25143c9b3108b56b248cb4ee1768c2b1eab882
Roman Vayvod <littlewhywhat@gmail.com>
2014-07-04 20:47:25 +0400

    SimpleDirectedGraph: added

---
+++ src/com/littlewhywhat/algorithms/graphs/SimpleDirectedGraph.java
@@ -0,0 +1,110 @@
+package com.littlewhywhat.algorithms.graphs;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class SimpleDirectedGraph<I extends Id> implements DirectedGraph<I> {
+
+	@Override
+	public void connect(int one, int two) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Iterator<I> getConnections(I item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public I get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(I arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends I> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator<I> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T[] toArray(T[] arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void reverse() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
