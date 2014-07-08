ff384dd5de21e34a1d3cb9e3907b99f994bdadda
Roman Vayvod <littlewhywhat@gmail.com>
2014-07-08 19:00:52 +0400

    Graph: added new generic parameter E extends Edge

--- src/com/littlewhywhat/algorithms/graphs/Graph.java
+++ src/com/littlewhywhat/algorithms/graphs/Graph.java
@@ -2,9 +2,9 @@ package com.littlewhywhat.algorithms.graphs;
 
 import java.util.Collection;
 
-public interface Graph<I, T extends Id<I>> extends Collection<T> {
-	Edge<I,T> connect(T one, T two);
+public interface Graph<I, T extends Id<I>, E extends Edge<I,T>> extends Collection<T> {
+	E connect(T one, T two);
 	T get(I id);
-	Collection<Edge<I,T>> getIn(T item);
-	Collection<Edge<I,T>> getOut(T item);
+	Collection<E> getIn(T item);
+	Collection<E> getOut(T item);
 }
