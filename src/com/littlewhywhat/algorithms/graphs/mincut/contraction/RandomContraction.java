f9b429fd596796aad7eac55acb7302efd97af208
Roman Vayvod <littlewhywhat@gmail.com>
2014-07-09 19:06:36 +0400

    RandomContraction: removed old, partly implemented new

--- src/com/littlewhywhat/algorithms/graphs/mincut/contraction/RandomContraction.java
+++ src/com/littlewhywhat/algorithms/graphs/mincut/contraction/RandomContraction.java
@@ -1,46 +1,36 @@
 package com.littlewhywhat.algorithms.graphs.mincut.contraction;
 
-import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 import com.littlewhywhat.algorithms.AbstractAlgorithm;
+import com.littlewhywhat.algorithms.graphs.Edge;
 import com.littlewhywhat.algorithms.graphs.Graph;
-import com.littlewhywhat.algorithms.graphs.Vertice;
-import com.littlewhywhat.algorithms.graphs.mincut.contraction.ContractionGraph.ContractionVertice;
+import com.littlewhywhat.algorithms.graphs.Id;
 
-public class RandomContraction extends
-		AbstractAlgorithm<Void, ContractionGraph, Integer> {
+public class RandomContraction<I, T extends Id<I>> extends
+		AbstractAlgorithm<Void, Graph<I, T, Edge<I, T>>, Integer> {
 
-	private Random random = new Random();
+	private final Random random = new Random();
 
 	@Override
 	public void execute() {
-		ContractionGraph graph = getData();
-		final LinkedList<Vertice> vertices = getVertices(graph);
+		final Graph<I, T, Edge<I, T>> graph = getData();
+		final List<Edge<I, T>> edges = graph.edges();
 		while (graph.size() != 2) {
-			ContractionVertice randomStart = getRandomVertice(vertices);
-			ContractionVertice randomEnd = getRandomConnection(graph, randomStart);
-			graph.merge(randomStart, randomEnd);
+			Edge<I, T> edge = getRandomEdge(edges);
+			merge(graph, edge);
 		}
-		setOutput(graph.getConnections(vertices.peek()).size());
+		setOutput(edges.size());
 	}
 
-	private ContractionVertice getRandomVertice(LinkedList<Vertice> vertices) {
-		ContractionVertice vertice = (ContractionVertice) vertices.get(random.nextInt(vertices.size()));
-		vertices.remove(vertice);
-		return vertice;
+	private void merge(Graph<I, T, Edge<I, T>> graph, Edge<I, T> edge) {
+		// TODO Auto-generated method stub
+		
 	}
 
-	private LinkedList<Vertice> getVertices(ContractionGraph graph) {
-		final LinkedList<Vertice> list = new LinkedList<Vertice>();
-		for (Vertice vertice: graph)
-			list.add(vertice);
-		return list;
-	}
-
-	private ContractionVertice getRandomConnection(Graph graph, Vertice vertice) {
-		return (ContractionVertice) ((ContractionGraph) graph).getConnections(vertice).get(random
-				.nextInt(((ContractionGraph) graph).getConnections(vertice).size())).getVertice();
+	private Edge<I, T> getRandomEdge(List<Edge<I, T>> edges) {
+		return edges.get(random.nextInt(edges.size()));
 	}
 
 }
