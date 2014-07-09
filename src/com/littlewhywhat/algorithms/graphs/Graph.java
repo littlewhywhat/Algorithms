89f57383cd56e88b2785299e957a1bead51c8f70
Roman Vayvod <littlewhywhat@gmail.com>
2014-07-09 15:23:00 +0400

    Graph: changed Collection to List in getIn, getOut, edges

--- src/com/littlewhywhat/algorithms/graphs/Graph.java
+++ src/com/littlewhywhat/algorithms/graphs/Graph.java
@@ -1,11 +1,12 @@
 package com.littlewhywhat.algorithms.graphs;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface Graph<I, T extends Id<I>, E extends Edge<I,T>> extends Collection<T> {
 	E connect(T one, T two);
 	T get(I id);
-	Collection<E> getIn(T item);
-	Collection<E> getOut(T item);
-	Collection<E> edges();
+	List<E> getIn(T item);
+	List<E> getOut(T item);
+	List<E> edges();
 }
