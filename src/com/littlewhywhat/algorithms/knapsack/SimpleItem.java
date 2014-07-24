98885707ed985339c2632469bd46582535c0f406
Roman Vayvod <littlewhywhat@gmail.com>
2014-07-24 18:54:49 +0100

    SimpleItem: added

---
+++ src/com/littlewhywhat/algorithms/knapsack/SimpleItem.java
@@ -0,0 +1,21 @@
+package com.littlewhywhat.algorithms.knapsack;
+
+public class SimpleItem implements KnapsackItem {
+
+	public SimpleItem(int value, int weight) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getValue() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
