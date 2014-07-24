648ef5430261e46356d4a8a591ffcfc6b157c7dd
Roman Vayvod <littlewhywhat@gmail.com>
2014-07-24 19:54:26 +0100

    TestKnapsackAlgo: added answers

--- src/com/littlewhywhat/algorithms/knapsack/test/TestKnapsackAlgo.java
+++ src/com/littlewhywhat/algorithms/knapsack/test/TestKnapsackAlgo.java
@@ -15,10 +15,14 @@ public class TestKnapsackAlgo {
 	private static final String INPUT = FOLDER + "knapsack.txt";
 	private static final String INPUT_SMALL = FOLDER + "knapsack_small.txt";
 	private static final String INPUT_BIG = FOLDER + "knapsack_big.txt";
+	
 	private static final KnapsackItem[] INPUT_DATA = new KnapsackItem[] {
 			new SimpleItem(3, 4), new SimpleItem(2, 3), new SimpleItem(4, 2),
 			new SimpleItem(4, 3) };
 	private static final int INPUT_CONFIG = 6;
+	private static final int ANSWER = 8;
+	private static final int ANSWER_SMALL = 2493893;
+	private static final int ANSWER_BIG = 0;
 	private KnapsackAlgo algo;
 	private KnapsackReader reader;
 
@@ -31,28 +35,28 @@ public class TestKnapsackAlgo {
 
 	@Test
 	public void test() {
-		test(INPUT);
+		test(INPUT, ANSWER);
 		Assert.assertArrayEquals(INPUT_DATA, algo.getData().toArray());
 		Assert.assertEquals(INPUT_CONFIG, algo.getConfig().intValue());
 	}
 
 	@Test
 	public void testSmall() {
-		test(INPUT_SMALL);
+		test(INPUT_SMALL, ANSWER_SMALL);
 	}
 
 	@Test
 	public void testBig() {
-		test(INPUT_BIG);
+		test(INPUT_BIG, ANSWER_BIG);
 	}
 
-	public void test(String inputFilePath) {
-		reader.setInputFilePath(INPUT);
+	public void test(String inputFilePath, int answer) {
+		reader.setInputFilePath(inputFilePath);
 		reader.read();
 		algo.setConfig(reader.getConfig());
 		algo.setData(reader.getData());
 		algo.execute();
-		System.out.println(algo.getOutput());
+		Assert.assertEquals(answer, algo.getOutput().intValue());
 	}
 
 }
