5a12148983d9e4f1cdbc09eed142408d468a1291
Roman Vayvod <littlewhywhat@gmail.com>
2014-07-24 18:54:30 +0100

    KnapsackAlgo: added

---
+++ src/com/littlewhywhat/algorithms/knapsack/KnapsackAlgo.java
@@ -0,0 +1,16 @@
+package com.littlewhywhat.algorithms.knapsack;
+
+import java.util.List;
+
+import com.littlewhywhat.algorithms.AbstractAlgorithm;
+
+public class KnapsackAlgo extends
+		AbstractAlgorithm<Integer, KnapsackItem[], List<KnapsackItem>> {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
