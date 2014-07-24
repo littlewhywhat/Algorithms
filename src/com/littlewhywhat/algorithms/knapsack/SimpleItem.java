264ad7d779b902806a869b676854788fa8168934
Roman Vayvod <littlewhywhat@gmail.com>
2014-07-24 19:18:26 +0100

    SimpleItem: added hashcode and equals

--- src/com/littlewhywhat/algorithms/knapsack/SimpleItem.java
+++ src/com/littlewhywhat/algorithms/knapsack/SimpleItem.java
@@ -24,4 +24,34 @@ public class SimpleItem implements KnapsackItem {
 		return "[v=" + value + ", w=" + weight + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + value;
+		result = prime * result + weight;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof SimpleItem)) {
+			return false;
+		}
+		SimpleItem other = (SimpleItem) obj;
+		if (value != other.value) {
+			return false;
+		}
+		if (weight != other.weight) {
+			return false;
+		}
+		return true;
+	}
+
 }
