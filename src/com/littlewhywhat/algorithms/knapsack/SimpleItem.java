5292a7c8a647e3b5bba6e1886b13fa94c1f6269b
Roman Vayvod <littlewhywhat@gmail.com>
2014-07-24 19:12:27 +0100

    SimpleItem: implemented

--- src/com/littlewhywhat/algorithms/knapsack/SimpleItem.java
+++ src/com/littlewhywhat/algorithms/knapsack/SimpleItem.java
@@ -2,20 +2,26 @@ package com.littlewhywhat.algorithms.knapsack;
 
 public class SimpleItem implements KnapsackItem {
 
+	private final int value;
+	private final int weight;
 	public SimpleItem(int value, int weight) {
-		// TODO Auto-generated constructor stub
+		this.value = value;
+		this.weight = weight;
 	}
 
 	@Override
 	public int getWeight() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.value;
 	}
 
 	@Override
 	public int getValue() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.weight;
+	}
+
+	@Override
+	public String toString() {
+		return "[v=" + value + ", w=" + weight + "]";
 	}
 
 }
