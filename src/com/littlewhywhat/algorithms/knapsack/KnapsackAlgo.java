810c314e62d22c97c3b1ed48dae7bc14ee2d84b2
Roman Vayvod <littlewhywhat@gmail.com>
2014-07-24 19:11:46 +0100

    KnapsackAlgo: changed data type to List<KnapsackItem>

--- src/com/littlewhywhat/algorithms/knapsack/KnapsackAlgo.java
+++ src/com/littlewhywhat/algorithms/knapsack/KnapsackAlgo.java
@@ -5,11 +5,10 @@ import java.util.List;
 import com.littlewhywhat.algorithms.AbstractAlgorithm;
 
 public class KnapsackAlgo extends
-		AbstractAlgorithm<Integer, KnapsackItem[], List<KnapsackItem>> {
+		AbstractAlgorithm<Integer, List<KnapsackItem>, List<KnapsackItem>> {
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
 		
 	}
 
