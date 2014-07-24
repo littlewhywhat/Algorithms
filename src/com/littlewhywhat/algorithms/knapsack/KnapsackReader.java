9fa3e9b5108727ca13fbfa4aa46d639a2ca99f98
Roman Vayvod <littlewhywhat@gmail.com>
2014-07-24 19:12:13 +0100

    KnapsackReader: changed Output type to List<KnapsackItem>

--- src/com/littlewhywhat/algorithms/knapsack/KnapsackReader.java
+++ src/com/littlewhywhat/algorithms/knapsack/KnapsackReader.java
@@ -1,19 +1,21 @@
 package com.littlewhywhat.algorithms.knapsack;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import com.littlewhywhat.algorithms.io.txt.TextFileInputReader;
 
-public class KnapsackReader extends TextFileInputReader<Integer, KnapsackItem[]> {
+public class KnapsackReader extends TextFileInputReader<Integer, List<KnapsackItem>> {
 
 	@Override
 	protected void extractInputData(Scanner scanner)
 			throws InputMismatchException {
 		setConfig(scanner.nextInt());
-		final KnapsackItem[] items = new KnapsackItem[scanner.nextInt()];
-		for (int i = 0; i < items.length; i++)
-			items[i] = new SimpleItem(scanner.nextInt(), scanner.nextInt());
+		final List<KnapsackItem> items = new ArrayList<KnapsackItem>(scanner.nextInt());
+		while(scanner.hasNext())
+			items.add(new SimpleItem(scanner.nextInt(), scanner.nextInt()));
 		setData(items);
 	}
 
