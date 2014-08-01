581414a59aef16482cc2a8b5d0883272354804e8
Roman Vayvod <littlewhywhat@gmail.com>
2014-08-01 20:07:47 +0100

    FloydWarshalAlgo: added

---
+++ src/com/littlewhywhat/algorithms/graphs/allpairs/FloydWarshalAlgo.java
@@ -0,0 +1,15 @@
+package com.littlewhywhat.algorithms.graphs.allpairs;
+
+import com.littlewhywhat.algorithms.AbstractAlgorithm;
+import com.littlewhywhat.algorithms.graphs.Graph;
+
+public class FloydWarshalAlgo extends
+		AbstractAlgorithm<Void, Graph, int[]> {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
