3f461f4fbe532644010b2c81000816066d0cf5c8
Roman Vayvod <littlewhywhat@gmail.com>
2014-08-07 19:13:03 +0100

    SalesmanAlgo: added private class CityNode

--- src/com/littlewhywhat/algorithms/salesman/SalesmanAlgo.java
+++ src/com/littlewhywhat/algorithms/salesman/SalesmanAlgo.java
@@ -7,6 +7,17 @@ import com.littlewhywhat.algorithms.AbstractAlgorithm;
 public class SalesmanAlgo extends
 		AbstractAlgorithm<Void, List<City>, Double> {
 
+	private class CityNode extends SimpleNode {
+		private final City city;
+		private CityNode(City city) {
+			this.city = city;
+		}
+		private City getCity() {
+			return this.city;
+		}
+	}
+	
+	
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
