660248a1b1d407ba53b3cfb9c14f0f4e29d6152a
Roman Vayvod <littlewhywhat@gmail.com>
2014-08-07 16:53:50 +0100

    CityReader: added

---
+++ src/com/littlewhywhat/algorithms/salesman/CityReader.java
@@ -0,0 +1,18 @@
+package com.littlewhywhat.algorithms.salesman;
+
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+import com.littlewhywhat.algorithms.io.txt.TextFileInputReader;
+
+public class CityReader extends TextFileInputReader<Void, List<City>> {
+
+	@Override
+	protected void extractInputData(Scanner scanner)
+			throws InputMismatchException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
