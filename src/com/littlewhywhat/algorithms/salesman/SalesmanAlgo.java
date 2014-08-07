128f394afb8a18643574113b2be49f8c17582cf3
Roman Vayvod <littlewhywhat@gmail.com>
2014-08-07 16:54:01 +0100

    SalesmanAlgo: added

---
+++ src/com/littlewhywhat/algorithms/salesman/SalesmanAlgo.java
@@ -0,0 +1,16 @@
+package com.littlewhywhat.algorithms.salesman;
+
+import java.util.List;
+
+import com.littlewhywhat.algorithms.AbstractAlgorithm;
+
+public class SalesmanAlgo extends
+		AbstractAlgorithm<Void, List<City>, Double> {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
