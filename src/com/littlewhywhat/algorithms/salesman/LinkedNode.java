83a8b7fb4e42b3e4f71a06521040073df1fa3ce1
Roman Vayvod <littlewhywhat@gmail.com>
2014-08-08 20:17:22 +0100

    LinkedNode: removed getChildren, added addBeforeLast, addAfterFirst

--- src/com/littlewhywhat/algorithms/salesman/LinkedNode.java
+++ src/com/littlewhywhat/algorithms/salesman/LinkedNode.java
@@ -1,14 +1,14 @@
 package com.littlewhywhat.algorithms.salesman;
 
-import java.util.List;
 
 public interface LinkedNode {
 	boolean hasParent();
 	LinkedNode getParent();
 	boolean hasChildren();
-	List<LinkedNode> getChildren();
 	LinkedNode getFirstChild();
 	LinkedNode getFirst();
 	LinkedNode getLast();
+	void addBeforeLast(LinkedNode node);
+	void addAfterFirst(LinkedNode node);
 }
  
