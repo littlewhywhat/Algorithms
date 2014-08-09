e9b3071aadfc31881cc9675fd52ce4cd072b35cf
Roman Vayvod <littlewhywhat@gmail.com>
2014-08-09 16:57:01 +0100

    SimpleLinkedNode: added

---
+++ src/com/littlewhywhat/algorithms/salesman/SimpleLinkedNode.java
@@ -0,0 +1,29 @@
+package com.littlewhywhat.algorithms.salesman;
+
+public class SimpleLinkedNode implements LinkedNode {
+
+	@Override
+	public LinkedNode getPrevious() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LinkedNode getNext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hasPrevious() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
