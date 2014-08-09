8efcfb99e245a9afd7d33656e67d2c2685c621d3
Roman Vayvod <littlewhywhat@gmail.com>
2014-08-09 16:57:14 +0100

    SimpleTreeLinkedNode: added

---
+++ src/com/littlewhywhat/algorithms/salesman/SimpleTreeLinkedNode.java
@@ -0,0 +1,41 @@
+package com.littlewhywhat.algorithms.salesman;
+
+public class SimpleTreeLinkedNode extends SimpleLinkedNode implements TreeLinkedNode {
+
+	@Override
+	public LinkedList<TreeLinkedNode> getChildren() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasChildren() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public TreeLinkedNode getParent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasParent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setPrevious(TreeLinkedNode node) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setNext(TreeLinkedNode node) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
