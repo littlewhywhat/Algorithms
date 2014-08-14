714ea940c301d16c1160668edb4dbf57fd76fa09
Roman Vayvod <littlewhywhat@gmail.com>
2014-08-14 20:01:25 +0100

    ClauseReader: added

---
+++ src/com/littlewhywhat/algorithms/twosat/ClauseReader.java
@@ -0,0 +1,17 @@
+package com.littlewhywhat.algorithms.twosat;
+
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+import com.littlewhywhat.algorithms.io.txt.TextFileInputReader;
+
+public class ClauseReader extends TextFileInputReader<Void, List<Clause>> {
+
+	@Override
+	protected void extractInputData(Scanner scanner)
+			throws InputMismatchException {
+		
+	}
+	
+}
