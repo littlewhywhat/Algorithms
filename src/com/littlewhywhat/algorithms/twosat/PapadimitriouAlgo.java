bc9d4e84a8f0a966e3c701a5730b5ff4cd83c15f
Roman Vayvod <littlewhywhat@gmail.com>
2014-08-14 20:01:56 +0100

    PapadimitriouAlgo: added

---
+++ src/com/littlewhywhat/algorithms/twosat/PapadimitriouAlgo.java
@@ -0,0 +1,16 @@
+package com.littlewhywhat.algorithms.twosat;
+
+import java.util.List;
+
+import com.littlewhywhat.algorithms.AbstractAlgorithm;
+
+public class PapadimitriouAlgo extends
+		AbstractAlgorithm<Void, List<Clause>, Boolean> {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
