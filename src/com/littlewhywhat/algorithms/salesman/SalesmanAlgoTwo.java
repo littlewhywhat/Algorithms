433479cfdf51389d164393764a6f6c24655c9db0
Roman Vayvod <littlewhywhat@gmail.com>
2014-08-14 19:42:35 +0100

    SalesmanAlgoTwo: implemented working dirty solution

--- src/com/littlewhywhat/algorithms/salesman/SalesmanAlgoTwo.java
+++ src/com/littlewhywhat/algorithms/salesman/SalesmanAlgoTwo.java
@@ -1,17 +1,12 @@
 package com.littlewhywhat.algorithms.salesman;
 
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 
 import com.littlewhywhat.algorithms.AbstractAlgorithm;
-import com.littlewhywhat.datastructure.Heap;
-import com.littlewhywhat.datastructure.SimpleHeap;
 
 public class SalesmanAlgoTwo extends
 		AbstractAlgorithm<Void, List<City>, Double> {
@@ -19,63 +14,67 @@ public class SalesmanAlgoTwo extends
 	
 	private LinkedHashMap<List<City>, int[]> cache = new LinkedHashMap<List<City>, int[]> ();
 	private LinkedHashMap<List<City>, int[]> current = new LinkedHashMap<List<City>, int[]>();
-	
-	private Heap<Integer> heapMin = SimpleHeap.getMinHeap(new Comparator<Integer>(){
-
-		@Override
-		public int compare(Integer one, Integer two) {
-			return one.compareTo(two);
-		}
-		
-	});
+	private int[][] distances;
 	
 	
 	@Override
 	public void execute() {
 		final List<City> cities = getData();
+		setDistances(cities);
 		final City firstCity = cities.remove(0);
 		int i = 1;
 		for (City city : cities) {
 			List<City> set = new ArrayList<City>();
 			set.add(city);
 			int[] answers = new int[cities.size() + 1];
-			//answers[i++] = firstCity.getDistanceTo(city);
+			answers[i++] = distances[city.getIndex()][firstCity.getIndex()];
 			cache.put(set, answers);
-			//cacheKeys.add(set);
 		}
-		for (int m = 0; m < cities.size(); m++) {
-			//System.out.println(cache);
+		for (int m = 0; m < cities.size() - 1; m++) {
 			long start = System.currentTimeMillis();
 			Iterator<List<City>> iterator = cache.keySet().iterator();
 			while (iterator.hasNext()) {
-				//System.out.println(data.list);
 				process(iterator.next() , cities);
 				iterator.remove();
 			}
-			
 			switchCurrentAndCache();
 			long end = System.currentTimeMillis();
-			//if (m == cities.size()-2)
-				System.out.println("iteration:" + m + " time:" + (end - start) + " keysSize:" + cache.size());
+			System.out.println("iteration:" + m + " time:" + (end - start) + " keysSize:" + cache.size());
 		}
-		System.out.println();
+		Iterator<List<City>> iterator = cache.keySet().iterator();
+		List<City> list = iterator.next();
+		int[] results = cache.get(list);
+		int min = Integer.MAX_VALUE;
+		for (City city : list) {
+			int temp = results[city.getIndex()] + distances[city.getIndex()][firstCity.getIndex()];
+			if (temp < min)
+				min = temp;
+		}
+		setOutput(((double)min)/100);
+		System.out.println(getOutput());
 	}
 
-	private void process(List<City> linkedList, List<City> cities) {
-		//int index = ((LinkedList<City>) linkedList).getLast().getIndex();
-		int index = linkedList.get(linkedList.size() - 1).getIndex();
+	private void setDistances(List<City> cities) {
+		distances = new int[cities.size()][cities.size()];
+		for (City city : cities) {
+			for (City cityOther : cities) {
+				distances[city.getIndex()][cityOther.getIndex()] = (int) (city.getDistanceTo(cityOther) * 100);
+			}
+		}
+	}
+
+	private void process(List<City> list, List<City> cities) {
+		int index = list.get(list.size() - 1).getIndex();
 		if (index < cities.size()) {
 			ListIterator<City> iterator = cities.listIterator(index);
 			while (iterator.hasNext())
-				addNewListToCurrent(linkedList, iterator.next());
+				addNewListToCurrent(list, iterator.next());
 		}
 	}
 
 	private void switchCurrentAndCache() {
 		cache = current;
 		current = new LinkedHashMap<List<City>, int[]>();
-		//cacheKeys = currentKeys;
-		//currentKeys = new ArrayList<LinkedList<City>>();
 	}
 
 	private void addNewListToCurrent(List<City> linkedList, City newCity) {
@@ -86,18 +85,19 @@ public class SalesmanAlgoTwo extends
 		ListIterator<City> listIterator = list.listIterator();
 		while (listIterator.hasNext()) {
 			City city = listIterator.next();
-			//listIterator.remove();
-			//Iterator<City> iterator = list.iterator();
-			//while (iterator.hasNext()) {
-			//	City k = iterator.next();
-			//	heapMin.insert(cache.get(list)[k.getIndex()] + k.getDistanceTo(city));
-			//}
-			//listIterator.add(city);
-			//hashMap[city.getIndex()] = heapMin.peek();
-			//((SimpleHeap<Integer>) heapMin).clear();
+			listIterator.remove();
+			Iterator<City> iterator = list.iterator();
+			int min = Integer.MAX_VALUE;
+			while (iterator.hasNext()) {
+				City k = iterator.next();
+				int temp = cache.get(list)[k.getIndex()] + distances[k.getIndex()][city.getIndex()];
+				if (temp < min)
+					min = temp;
+			}
+			listIterator.add(city);
+			hashMap[city.getIndex()] = min;
 		}
 		current.put(list, hashMap );
-		//currentKeys.add(list);
 	}
 
 }
