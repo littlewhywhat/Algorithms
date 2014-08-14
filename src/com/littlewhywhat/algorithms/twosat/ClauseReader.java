95ecdbc969ca010a836e15f6afdc2502a342c617
Roman Vayvod <littlewhywhat@gmail.com>
2014-08-14 21:47:06 +0100

    ClauseReader: implemented, changed config to Integer and data to List<SimpleClause>

--- src/com/littlewhywhat/algorithms/twosat/ClauseReader.java
+++ src/com/littlewhywhat/algorithms/twosat/ClauseReader.java
@@ -1,17 +1,25 @@
 package com.littlewhywhat.algorithms.twosat;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
 import com.littlewhywhat.algorithms.io.txt.TextFileInputReader;
 
-public class ClauseReader extends TextFileInputReader<Void, List<Clause>> {
+public class ClauseReader extends TextFileInputReader<Integer, List<SimpleClause>> {
 
 	@Override
 	protected void extractInputData(Scanner scanner)
 			throws InputMismatchException {
-		
+		setConfig(scanner.nextInt());
+		final List<SimpleClause> data = new ArrayList<SimpleClause>();
+		while (scanner.hasNext()) {
+			int one = scanner.nextInt();
+			int two = scanner.nextInt();
+			data.add(new SimpleClause(one < 0, Math.abs(one) - 1, two < 0, Math.abs(two) - 1));
+		}
+		setData(data);
 	}
 	
 }
