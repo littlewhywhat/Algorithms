db20b3e5af6ae2134ffa8d3e120c1feed2ea5637
Roman Vayvod <littlewhywhat@gmail.com>
2014-08-16 13:49:37 +0100

    PapadimitriouAlgoTest: added testTest, changed string constants to be static

--- src/com/littlewhywhat/algorithms/twosat/test/PapadimitriouAlgoTest.java
+++ src/com/littlewhywhat/algorithms/twosat/test/PapadimitriouAlgoTest.java
@@ -9,10 +9,11 @@ import com.littlewhywhat.algorithms.twosat.ClauseReader;
 import com.littlewhywhat.algorithms.twosat.PapadimitriouAlgo;
 
 public class PapadimitriouAlgoTest {
-
-	private final String FOLDER = "src/com/littlewhywhat/algorithms/twosat/test/input/";
-	private final String INPUT_SMALL = FOLDER + "2sat0.txt";
-	private final String INPUT = FOLDER + "2sat0.txt";
+	
+	private static final String FOLDER = "src/com/littlewhywhat/algorithms/twosat/test/input/";
+	private static final String INPUT_TEST = FOLDER + "test.txt";
+	private static final String INPUT_SMALL = FOLDER + "2sat0.txt";
+	private static final String INPUT = FOLDER + "2sat0.txt";
 	private PapadimitriouAlgo algo;
 	private ClauseCleaner cleaner;
 	private ClauseReader reader;
@@ -42,6 +43,11 @@ public class PapadimitriouAlgoTest {
 	public void testSmall() {
 		for (int i = 0; i < 10; i++)
 			test(INPUT_SMALL, true);
+	}
+	
+	@Test
+	public void testTest() {
+		test(INPUT_TEST, false);
 	}
 
 }
