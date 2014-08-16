cb3b252119e0528b9d4ad8a0ae72326a0bfef79a
Roman Vayvod <littlewhywhat@gmail.com>
2014-08-16 12:36:41 +0100

    ClauseCleaner: added with implemented isAboutOneVariable, isDuplicate

---
+++ src/com/littlewhywhat/algorithms/twosat/ClauseCleaner.java
@@ -0,0 +1,84 @@
+package com.littlewhywhat.algorithms.twosat;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Set;
+
+import com.littlewhywhat.algorithms.AbstractAlgorithm;
+
+public class ClauseCleaner extends
+		AbstractAlgorithm<Integer, List<SimpleClause>, List<SimpleClause>> {
+
+	private Set<Clause> bloomFilter = new HashSet<Clause>();
+	private Map<Integer, LinkedList<Clause>> clausesCount = new HashMap<Integer, LinkedList<Clause>>();
+	private int i;
+
+	@Override
+	public void execute() {
+		final List<SimpleClause> data = getData();
+		for (ListIterator<SimpleClause> listIterator = data.listIterator(); listIterator
+				.hasNext();) {
+			SimpleClause clause = listIterator.next();
+			if (isDuplicate(clause)
+					|| (isAboutOneVariable(clause) && (clause
+							.isInversedOneOrTwo()
+							|| clause.isInversedOneAndTwo() || clause
+								.isNotInversedOneAndTwo())))
+				listIterator.remove();
+			else {
+				int hash = getIndicesHash(clause);
+				if (!clausesCount.containsKey(hash))
+					clausesCount.put(hash, new LinkedList<Clause>());
+				clausesCount.get(hash).add(clause);
+			}
+
+		}
+		for (Iterator<LinkedList<Clause>> iterator = clausesCount.values().iterator(); iterator.hasNext();) {
+			if (iterator.next().size() < 3)
+				iterator.remove();
+		}
+		System.out.println("i= " + i);
+		setOutput(data);
+	}
+
+	private void countClauses(int variableIndexOne, int variableIndexTwo) {
+		//clausesCount[variableIndexOne][variableIndexTwo] += 1;
+		//if (clausesCount[variableIndexOne][variableIndexTwo] > 2)
+			//System.out.println(variableIndexOne + " " + variableIndexTwo);
+	}
+
+	private boolean isAboutOneVariable(SimpleClause clause) {
+		if (clause.getVariableIndexOne() == clause.getVariableIndexTwo())
+			return true;
+		return false;
+	}
+
+	private int getIndicesHash(SimpleClause clause) {
+		int lessIndex;
+		int moreIndex;
+		final int prime = 31;
+		int result = 1;
+		if (clause.getVariableIndexOne() < clause.getVariableIndexTwo()) {
+			lessIndex = clause.getVariableIndexOne();
+			moreIndex = clause.getVariableIndexTwo();
+		} else {
+			lessIndex = clause.getVariableIndexTwo();
+			moreIndex = clause.getVariableIndexOne();
+		}
+		result = prime * result + lessIndex;
+		result = prime * result + moreIndex;
+		return result;
+	}
+
+	private boolean isDuplicate(Clause clause) {
+		if (bloomFilter.contains(clause))
+			return true;
+		bloomFilter.add(clause);
+		return false;
+	}
+}
