cfb48267c5fcb6b08422faf5a8a9d5b8f4edecb5
Roman Vayvod <littlewhywhat@gmail.com>
2014-08-16 12:37:17 +0100

    PapadimitriouAlgoTest: added ClauseCleaner

--- src/com/littlewhywhat/algorithms/twosat/test/PapadimitriouAlgoTest.java
+++ src/com/littlewhywhat/algorithms/twosat/test/PapadimitriouAlgoTest.java
@@ -3,30 +3,38 @@ package com.littlewhywhat.algorithms.twosat.test;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.littlewhywhat.algorithms.twosat.ClauseCleaner;
 import com.littlewhywhat.algorithms.twosat.ClauseReader;
 import com.littlewhywhat.algorithms.twosat.PapadimitriouAlgo;
 
 public class PapadimitriouAlgoTest {
 
 	private final String FOLDER = "src/com/littlewhywhat/algorithms/twosat/test/input/";
-	private final String INPUT = FOLDER + "2sat0.txt";
+	private final String INPUT = FOLDER + "2sat5.txt";
 	private PapadimitriouAlgo algo;
+	private ClauseCleaner cleaner;
 	private ClauseReader reader;
 	
 	@Before
 	public void setUp() throws Exception {
 		algo = new PapadimitriouAlgo();
 		reader = new ClauseReader();
+		cleaner = new ClauseCleaner();
 	}
 
 	@Test
 	public void test() {
 		reader.setInputFilePath(INPUT);
 		reader.read();
-		algo.setConfig(reader.getConfig());
-		algo.setData(reader.getData());
-		algo.execute();
-		System.out.println(algo.getOutput());
+		System.out.println(reader.getData().size());
+		cleaner.setConfig(reader.getConfig());
+		cleaner.setData(reader.getData());
+		cleaner.execute();
+		System.out.println(cleaner.getOutput().size());
+		//algo.setConfig(reader.getConfig());
+		//algo.setData(cleaner.getData());
+		//algo.execute();
+		//System.out.println(algo.getOutput());
 	}
 
 }
